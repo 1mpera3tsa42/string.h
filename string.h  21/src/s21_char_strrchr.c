509da94fd91f4d9cfe8fac64a63ac97694f5417d
Babs Star <babsstar@il-h6.msk.21-school.ru>
2024-02-12 20:04:23 +0300

    goodbye

--- string.h  21/src/s21_char_strrchr.c
+++ string.h  21/src/s21_char_strrchr.c
@@ -3,11 +3,11 @@
 char *s21_strchr(const char *str, int c) {
   char *res = S21_NULL;
   int a = s21_strlen(str) + 1;
-  for  (int i = 0; i < x; i++) {
-    if (str[i] == c){
-      res  = (str + 1);
+  for (int i = 0; i < x; i++) {
+    if (str[i] == c) {
+      res = (str + 1);
       break;
     }
   }
-  return (char*)res;
+  return (char *)res;
 }
